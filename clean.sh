8e38b112dbfc2e56fd0e44b9f51f7f286a293f9b
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2020-10-25 15:30:39 +1100

    Change directory structure

--- clean.sh
+++ clean.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-find . -type f -name '*.exe' -delete
-find . -type f -name '*.txt' -delete
+if [ $# -eq 0 ]
+then
+    find ./lib/ -type f -name '*.dll' -delete
+else
+    while [ $# -ne 0 ]
+    do
+        ARG="$1"
+        shift
+        case "$ARG" in
+        all)
+            find ./bin/ -type f -name '*.exe' -delete
+            find ./lib/ -type f -name '*.dll' -delete
+            ;;
+        esac
+    done  
+fi
